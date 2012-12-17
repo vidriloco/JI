30e8e1a388eccb8b3fcfe2e03755977d8d013695
Alejandro Cruz Paz <vidriloco@gmail.com>
2012-12-16 21:42:11 -0600

    Primer commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ji::Application.config.secret_token = 'cb8e2f6f25d1d39bd4220877ff4ab1f457b5c2f2ee0645d7ca82bea377b8e640ae8aab9c142073c501df1d5d05857668fe09b94fadeb75f9477386e323a97da1'
